9889cd900f174130b94a35e17120848759743be3
Jaysee <96378356+JAYSEECODES@users.noreply.github.com>
2024-03-29 12:34:59 +0300

    Update 0.1

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 var start = 0;
 var energy = 250;
 
+document.getElementById('bubble').setAttribute('draggable', false);
 document.getElementById('bubble').onclick = counterAdd
 
 function counterAdd() {
     if (energy > -1) {
     document.getElementById('amount').innerText = start++;
     document.getElementById('energy-amount').innerText = energy--;
+    $("#bubble").animate({
+        'width': 235,
+        'height': 216
+    }, 50, function() { 
+        $("#bubble").animate({
+            'width': 246,
+            'height': 227
+        }, 50,)
+    })
     } else {
         document.getElementById('amount').innerText = 'Wait!';
         setTimeout(function() {
