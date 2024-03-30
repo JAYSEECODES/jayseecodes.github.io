330db8f684e9f472d3efab334b85ddc57cb32e7a
Jaysee <96378356+JAYSEECODES@users.noreply.github.com>
2024-03-30 15:26:10 +0300

    Update 0.21

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ function counterAdd() {
         }
         document.getElementById('amount').innerText = start++;
         document.getElementById('energy-amount').innerText = energy--;
-        $('body').append($("<div/>").attr("id", "flyingtext").addClass("flyingtext").html("<div>+1</div>").animate({
+        navigator.vibrate([100]);
+        $('body').append($("<div/>").attr("id", "flyingtext").addClass("flyingtext").html("<div>+1</div>").css({"top": Math.random() * (450 - 300) + 300, "margin-left": Math.random() * (250 - -250) + -250}).animate({
             opacity: "0",
             'line-height':'90px'
           }, 200 , function() {
