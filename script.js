63b22fb7c319b278d7444fbfb0eeb0136f57a449
Jaysee <96378356+JAYSEECODES@users.noreply.github.com>
2024-08-05 23:15:22 +0300

    Visuals and Functionality Update - Version 0.15

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-tg.setHeaderColor("#000000");
-tg.setBackgroundColor("#000000")
+tg.setHeaderColor("#0E0415");
+tg.setBackgroundColor("#0E0415")
+
+const cards = document.getElementsByClassName("card");
+
+const tags = document.getElementsByClassName("tag-item");
+
+
+function SlideUp(cards_object) {
+    step = 0;
+    for (let i = 0; i < cards_object.length; i++) {
+        setTimeout(function() {
+            $(cards_object[i]).delay(step).addClass('slidein');
+        }, step);
+        step += 50;
+      }
+}
+
+
+$(".tag-item").click(function(){
+    tg.HapticFeedback.impactOccurred('medium');
+    if ($(this).hasClass('selected') == false) {
+        for (let i = 0; i < tags.length; i++) {
+            if ($(tags[i]).hasClass('selected') == true) {
+                $(tags[i]).removeClass('selected');
+            }
+        }
+        $(this).addClass('selected');
+    }
+});
+
+
+SlideUp(cards)
